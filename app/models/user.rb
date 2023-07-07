b78239dfa95bb3f9b4f18e28f5dfedc8e4a0e147
Godwin <godwin.yampoi@student.moringaschool.com>
2023-07-07 07:08:04 +0300

    workouts controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
     has_many :workouts
     has_many :exercises, through: :user_exercises
 
-    has_secure_password
+    #has_secure_password
 
     validates :username, presence: true, uniqueness: true
     validates :email, presence: true, uniqueness: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# seeds.rb
+
+# Create Users
+user = User.create(username: 'Godwin', email: 'godwin449@gmail.com', password_digest: 'password1')
+user = User.create(username: 'Glen', email: 'glen449@gmail.com', password_digest: 'password2')
+user = User.create(username: 'Hellen', email: 'hellen449@gmail.com', password_digest: 'password3')
+
+# # Create Workouts
+# workout = Workout.create(workout_name: 'push-ups')
+# workout = Workout.create(workout_name: 'sit-ups')
+# workout = Workout.create(workout_name: 'shoulder_press')
